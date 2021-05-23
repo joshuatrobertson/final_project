fbe4e7730961e467bca9110d9aec36e1c64bfb06
Josh <joshuatrobertson@protonmail.com>
2021-05-23 20:58:22 +0100

    Added top 2 feature section

--- uber_haircuts/lib/widgets/featured.dart
+++ uber_haircuts/lib/widgets/featured.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:uber_haircuts/models/parent_barber.dart';
+import 'package:uber_haircuts/widgets/return_text.dart';
+import '../common_items.dart';
+
+List<ParentBarber> featuredParentsBarbers = [
+  ParentBarber(name: "Downtown Barbers", image: "barber03", rating: 4.6),
+  ParentBarber(name: "High Street Barbers", image: "barber04", rating: 4.9)
+];
+
 
 class Featured extends StatelessWidget {
   const Featured({Key key}) : super(key: key);
@@ -7,17 +17,28 @@ class Featured extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 4,
+        itemCount: featuredParentsBarbers.length,
         itemBuilder: (_, index){
           return Container(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
             height: 200,
             width: 200,
-            child: Column(
-              children: <Widget>[
+            child: Stack(
+              children:[
+                Container(
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/images/${featuredParentsBarbers[index].image}.jpg", height: 120, width: 200,
+                      fit: BoxFit.cover,
+                    )
+                ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                  child: Image.asset("assets/images/barber04.jpg", height: 200, width: 200,),
-                )
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: Container(
+
+                    alignment: Alignment.bottomCenter,
+                    child: ReturnText(text: featuredParentsBarbers[index].name, size: 15, color: white,),
+                  ),
+                ),
               ],
             ),
           );
