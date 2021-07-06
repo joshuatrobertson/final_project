a52e8046b2e4f7dc1fb126364b1b330838c04f77
Josh <joshuatrobertson@protonmail.com>
2021-07-06 11:34:06 +0100

    implemented and added scaffold for maps api, need to add to user_gps.dart

--- uber_haircuts/lib/screens/user_gps.dart
+++ uber_haircuts/lib/screens/user_gps.dart
@@ -1,11 +1,16 @@
+// Documentation here - https://developers.google.com/maps/documentation/places/web-service/autocomplete
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber_haircuts/models/location.dart';
 import 'package:uber_haircuts/widgets/categories_filter.dart';
 import 'package:uber_haircuts/widgets/return_text.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 import '../common_items.dart';
 
+final _controller = TextEditingController;
 
 class UserGPS extends StatelessWidget {
 
@@ -33,7 +38,8 @@ class _FireMapState extends State<FireMap> {
         GoogleMap(initialCameraPosition: CameraPosition(
           target: LatLng(51.529395551477165, -0.5769402978782877),
           zoom: 2,
-        )),
+          )
+        ),
         SizedBox(height: 5,),
         // Search bar
         Padding(
@@ -41,8 +47,8 @@ class _FireMapState extends State<FireMap> {
           child: Container(
             decoration: BoxDecoration(
               border: Border.all(
-                  color: theme,
-                  width: 3
+                  color: lightGrey,
+                  width: 2
               ),
               borderRadius: BorderRadius.circular(14),
               color: lightGrey,
@@ -52,16 +58,13 @@ class _FireMapState extends State<FireMap> {
                 onTap: () async {
                   showSearch(
                     context: context,
-                    // we haven't created AddressSearch class
-                    // this should be extending SearchDelegate
                     delegate: ShowSearchPage(),
                   );
                 },
                 decoration: InputDecoration(
                   hintText: "Enter your address",
-                  border: InputBorder.none,
+                  border: InputBorder.none
                 ),
-                // Add search text to the search bar and remove the border
               ),
               // Add the search icon and colour according to the theme
               leading: Icon(Icons.search, color: accent_1, size: 25),
@@ -107,13 +110,10 @@ class ShowSearchPage extends SearchDelegate<LocationModel> {
     return StreamBuilder(
       builder: (context, snapshot) => query == ''
           ? Container(
-        padding: EdgeInsets.fromLTRB(20.0, 15, 10, 10),
-        child: Text('Enter your address'),
       )
           : snapshot.hasData
           ? ListView.builder(
         itemBuilder: (context, index) => ListTile(
-          // we will display the data returned from our future here
           title:
           Text(snapshot.data[index]),
           onTap: () {
@@ -124,4 +124,38 @@ class ShowSearchPage extends SearchDelegate<LocationModel> {
       )
           : CircularProgressIndicator);
   }
+}
+
+class GoogleMapsAPI {
+  var client = http.Client();
+
+  GoogleMapsAPI(this.sessionToken, this.client);
+
+  final sessionToken;
+
+  // Locations API key
+  static final String apiKey = 'AIzaSyADHRmQGzWbsWIoebuHjFSuBPM77bIdvXs';
+
+  Future<List<LocationModel>> getLocations(String input) async {
+    // Create an api request using the session token/ api key
+    final apiRequest =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$input&types=address&components=country:ch&lang=en&key=$apiKey&sessiontoken=$sessionToken';
+    // Send a get request to the server and return a response
+    final response = await client.get(Uri.parse(apiRequest));
+    final result = json.decode(response.body);
+      if (result['status'] == 'OK') {
+        // If we are returned results, put them into a list
+        return result['predictions']
+            .map<LocationModel>((p) => LocationModel(p['place_id'], p['description']))
+            .toList();
+      }
+      // Successful call but no results
+      if (result['status'] == 'ZERO_RESULTS') {
+        return [];
+      }
+      else {
+        throw Exception(result['error_message']);
+      }
+  }
+  
 }
